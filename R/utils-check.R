bc0ce92af452c90d5d25e73eb8ae35d9a0dba9fb
Eli Pousson <eli.pousson@gmail.com>
2022-06-28 00:29:52 -0400

    feat: add check_sf function

--- R/utils-check.R
+++ R/utils-check.R
@@ -1,5 +1,4 @@
 check_null <- function(x = NULL, arg = caller_arg(x), null.ok = FALSE, null.req = FALSE, ...) {
-
   if (null.req) {
     null.ok <- null.req
   }
@@ -24,7 +23,8 @@ check_character <- function(x = NULL, arg = caller_arg(x), null.ok = FALSE, ...)
 
   cli_abort(
     c("{.arg {arg}} must be a character vector.",
-      "i" = "You've supplied a {class(x)} object."),
+      "i" = "You've supplied a {class(x)} object."
+    ),
     ...
   )
 }
@@ -38,7 +38,8 @@ check_len <- function(x = NULL, len = 1, arg = caller_arg(x), null.ok = FALSE, .
 
   cli_abort(
     c("{.arg {arg}} must be length {len}.",
-      "i" = "You've supplied a length {length(x)} object."),
+      "i" = "You've supplied a length {length(x)} object."
+    ),
     ...
   )
 }
@@ -69,7 +70,8 @@ check_starts_with <- function(x = NULL, string = NULL, arg = caller_arg(x), null
   if (is.null(message)) {
     cli_abort(
       c("{.arg {arg}} must start with {.val {string}}.",
-        "i" = "The provided string is {.val {x}}."),
+        "i" = "The provided string is {.val {x}}."
+      ),
       ...
     )
   }
@@ -87,7 +89,8 @@ check_df <- function(x, arg = caller_arg(x), null.ok = FALSE, ...) {
 
   cli_abort(
     c("{.arg {arg}} must be a data frame.",
-      "i" = "You've supplied a {class(x)} object."),
+      "i" = "You've supplied a {class(x)} object."
+    ),
     ...
   )
 }
@@ -103,7 +106,8 @@ check_df_rows <- function(x, rows = 1, arg = caller_arg(x), null.ok = FALSE, ...
 
   cli_abort(
     c("{.arg {arg}} must have {rows} row(s) or more.",
-      "i" = "You've supplied a data frame with {nrow(x)} rows."),
+      "i" = "You've supplied a data frame with {nrow(x)} rows."
+    ),
     ...
   )
 }
@@ -119,7 +123,30 @@ check_df_paper <- function(x, cols = c("width", "height", "orientation", "units"
 
   cli_abort(
     c("{.arg {arg}} must be a data frame with columns named {cols}.",
-      "i" = "You've supplied a data frame that is missing {cols[cols %in% names(x)]}."),
+      "i" = "You've supplied a data frame that is missing {cols[cols %in% names(x)]}."
+    ),
+    ...
+  )
+}
+
+#' @noRd
+check_sf <- function(x, arg = caller_arg(x), null.ok = FALSE, ext = FALSE, ...) {
+  check_null(x, arg, null.ok)
+
+  if (overedge::is_sf(x, ext = ext)) {
+    invisible(return(TRUE))
+  }
+
+  if (ext) {
+    sf <- c("sf", "sfc", "bbox")
+  } else {
+    sf <- "sf"
+  }
+
+  cli_abort(
+    c("{.arg {arg}} must be a {.code sf} object.",
+      "i" = "You've supplied a {class(x)} object."
+    ),
     ...
   )
 }
