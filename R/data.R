68e5ac2982cf845e47f3da879c295404f1e19cbc
Eli Pousson <eli.pousson@gmail.com>
2022-06-28 23:33:08 -0400

    feat: add osm_building_tags to exported data

--- R/data.R
+++ R/data.R
@@ -1,3 +1,12 @@
+#' OpenStreetMap building tags
+#'
+#' Used by [get_osm_data] if key = "building".
+#'
+#' More information on the building key
+#' <https://wiki.openstreetmap.org/wiki/Key:building>
+#'
+#' @format A character vector with length of 84
+"osm_building_tags"
 
 #' U.S. State boundaries (1:5 mi scale, bbox and wkt)
 #'
