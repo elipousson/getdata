a28557515776089952223c15a7d472f2213b3f38
Eli Pousson <eli.pousson@gmail.com>
2022-07-05 14:07:11 -0400

    docs: update docs + examples

--- examples/format_sf_data.R
+++ examples/format_sf_data.R
@@ -14,15 +14,9 @@ plot(nc_county_simple, max.plot = 1)
 
 # Erase data
 nc_co_water <- get_tigris_data(type = "area water", state = "NC", county = nc_county$NAME)
-plot(format_sf_data(nc_county, erase_data = nc_co_water), max.plot = 1)
-
-# Relocate sf column with a helper function function
-relocate_sf_col(nc, .after = contains("AREA"))[,c(1, 2, 6)]
+nc_county_erased <- format_sf_data(nc_county, erase_data = nc_co_water)
+plot(nc_county_erased, max.plot = 1)
 
 # If sf_req is set to FALSE, use any object that can be converted with sfext::as_sf
 nc_bbox <- st_bbox(nc)
 plot(format_sf_data(nc_bbox, erase_data = nc_county_simple, sf_req = FALSE))
-
-# Bind area units (converting square meters to square miles)
-bind_units_col(nc, st_area(nc), units = "mi^2")[,c(1,5,15)]
-
