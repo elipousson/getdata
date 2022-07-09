b6475cfbbfc2cd9d11524b86b5e988940467abb3
Eli Pousson <eli.pousson@gmail.com>
2022-07-09 15:08:08 -0400

    feat: add make_location_grid

---
+++ R/make_location_grid.R
@@ -0,0 +1,26 @@
+#' Make a grid over the bounding box of a location
+#'
+#' If location is a single feature sf object, the original columns of the object
+#' are included in the output grid. If location has mutiple features, the values
+#' of name_col are combined with [sfext::st_union_ext] and other columns are
+#' dropped. The input sf object should not have columns named id, rows, or cols.
+#'
+#' @param location A sf, sfc, or bbox object passed to [sfext::st_make_grid_ext]
+#' @inheritParams sfext::st_union_ext
+#' @inheritDotParams sfext::st_make_grid_ext
+#' @export
+make_location_grid <- function(location,
+                               name_col = "name",
+                               ...) {
+  grid <- sfext::st_make_grid_ext(x = location, ...)
+
+  location <- sfext::st_union_ext(location, name_col = name_col)
+
+  grid <-
+    dplyr::bind_cols(
+      sf::st_drop_geometry(location),
+      grid
+    )
+
+  sfext::df_to_sf(grid)
+}
