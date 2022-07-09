2f1cc3e83d59461568d5dded8f1aa753419389fb
Eli Pousson <eli.pousson@gmail.com>
2022-07-09 15:12:29 -0400

    refactor: expand imports from sfext

--- R/getdata-package.R
+++ R/getdata-package.R
@@ -1,7 +1,7 @@
 #' @import rlang
 #' @importFrom glue glue glue_collapse
 #' @importFrom cli cli_abort cli_inform cli_warn
-#' @importFrom sfext st_bbox_ext df_to_sf is_sf
+#' @importFrom sfext st_bbox_ext st_filter_ext df_to_sf is_sf as_sf relocate_sf_col rename_sf_col
 #' @keywords internal
 "_PACKAGE"
 
