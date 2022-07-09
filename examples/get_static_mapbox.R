ffc5591d2b0b64a0845e04116c75f53824a616e8
Eli Pousson <eli.pousson@gmail.com>
2022-07-09 15:11:20 -0400

    docs: add examples for get_static_mapbox

---
+++ examples/get_static_mapbox.R
@@ -0,0 +1,22 @@
+\dontrun{
+  get_osm_static_mapbox(
+    id = "way/49664223",
+    dist = 0.5,
+    unit = "mi",
+    overlay_style = list(
+      stroke = "darkgreen",
+      fill = "green",
+      fill_opacity = 0.25
+    )
+  )
+
+  nc <- read_sf_path(system.file("shape/nc.shp", package = "sf"))
+
+  get_location_static_mapbox(
+    type = nc,
+    name = "Ashe",
+    name_col = "NAME",
+    dist = 50,
+    unit = "mi"
+  )
+}
