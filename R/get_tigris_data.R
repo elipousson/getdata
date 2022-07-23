c90266167657e8c0e00130e7d647be8caef5caf8
Eli Pousson <eli.pousson@gmail.com>
2022-07-23 00:04:03 -0400

    feat: add name_col and cache parameters

--- R/get_tigris_data.R
+++ R/get_tigris_data.R
@@ -1,7 +1,8 @@
-#' Use {tigris} to get state-level data from the U.S. Census Bureau
+#' Use tigris to get state-level data from the U.S. Census Bureau
 #'
-#' Use the tigris package to download state-level data from the U.S. Census
-#' Bureau API and filter by name or GeoID.
+#' Use the [{tigris}](https://github.com/walkerke/tigris) package to download
+#' state-level data from the U.S. Census Bureau API and optionally filter by
+#' name or GeoID.
 #'
 #' @details Supported data types:
 #'
@@ -20,30 +21,40 @@
 #'
 #' @param state State name, abbreviation, or GeoID. Required. Defaults to
 #'   getOption("getdata.state").
-#' @param name Name matching a value in the namelsad, namelsad, or geoid
-#'   columns. Default: `NULL`
+#' @param name,name_col Name and columns to filter by name. name defaults to
+#'   `NULL`, and name_col defaults to c("namelsad", "namelsad", "geoid")
+#'   columns.
 #' @param type Type of data to return, Default: `NULL`; See details for
 #'   supported options.
-#' @param cb If cb is set to `TRUE`, download a generalized (1:500k) tracts
-#'   file. If `FALSE`, download the most detailed TIGER/Line file. Defaults to
-#'   `TRUE` (reverse of the default for [tigris] functions). This parameter is
-#'   *not* used when type is set to blocks, roads, primary secondary roads, area
-#'   water, linear water, landmarks, or zctas.
+#' @param cb If `TRUE`, download a generalized (1:500k) file. If `FALSE`,
+#'   download the most detailed TIGER/Line file. Defaults to `TRUE` (reverse of
+#'   the default for tigris functions). This parameter is *not* used when type
+#'   is set to blocks, roads, primary secondary roads, area water, linear water,
+#'   landmarks, or zctas.
+#' @param cache If `TRUE`, set `options(tigris_use_cache = TRUE)` to cache
+#'   downloaded tigris data. Ignored if `getOption("tigris_use_cache")` is not
+#'   `NULL`.
 #' @inheritParams format_sf_data
 #' @inheritParams format_data
-#' @param ... Additional parameters passed on to {tigris} functions.
+#' @param ... Additional parameters passed on to tigris functions.
 #' @return A simple feature object matching the type provided.
 #' @rdname get_state_tigris
 #' @export
 get_tigris_data <- function(type = NULL,
                             state = getOption("getdata.state"),
                             name = NULL,
+                            name_col = c("name", "namelsad", "geoid"),
                             crs = getOption("getdata.crs", default = 3857),
                             cb = TRUE,
                             clean_names = TRUE,
+                            cache = TRUE,
                             ...) {
   if (!is.null(type)) {
     type <- tolower(type)
+  }
+
+  if (cache && is.null(getOption("tigris_use_cache"))) {
+    options(tigris_use_cache = TRUE)
   }
 
   is_pkg_installed("tigris")
@@ -90,17 +101,25 @@ get_tigris_data <- function(type = NULL,
     return(data)
   }
 
-  data[lookup_tigris_name(name, data), ]
+  data[lookup_tigris_name(name, name_col, data), ]
 }
 
-#' Help
+#' Helper to filter rows with name values in name_cols
+#'
 #' @noRd
-lookup_tigris_name <- function(name, data = NULL) {
-  name <- tolower(name)
+lookup_tigris_name <- function(name, name_col = c("name", "namelsad", "geoid"), data = NULL) {
 
-  lookup_col <- function(name_col) {
-    tolower(data[[name_col]]) %in% name
+  lookup <- FALSE
+  name <- tolower(name)
+  name_col <- name_col[name_col %in% names(data)]
+
+  lookup_col <- function(x) {
+    tolower(data[[x]]) %in% name
   }
 
-  lookup_col("name") | lookup_col("namelsad") | lookup_col("geoid")
+  for (x in name_col) {
+    lookup <- lookup | lookup_col(x)
+  }
+
+  lookup
 }
