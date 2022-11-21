3da03c886ae1df042ac481c4f0f58b2e98f9db95
Eli Pousson <eli.pousson@gmail.com>
2022-11-21 12:52:05 -0500

    feat(set_access_token): allow named list for token
    - feat(set_access_token): allow named list for token
    - refactor: update NAMESPACE + move tidyr to Imports

--- R/set_access_token.R
+++ R/set_access_token.R
@@ -1,83 +1,95 @@
 #' Set or get an access token or API key to/from environment variables.
 #'
-#' Based on the [mapboxapi::mb_access_token] function from the {mapboxapi}
+#' Based on the [mapboxapi::mb_access_token()] function from the {mapboxapi}
 #' package by Kyle Walker.
 #'
 #' @param token An access token or API key; required for [set_access_token()].
-#'   If token is not provided; type is required for [get_access_token()].
+#'   If token is not provided; type is required for [get_access_token()]. Length
+#'   1 character vector or list. If named, the name of the token is used in
+#'   place of type.
 #' @param overwrite If `TRUE`, overwrite existing token; Default: `FALSE`
 #' @param install If `TRUE`, install token for use in future sessions; Default:
 #'   `FALSE`
-#' @param type Name of token; defaults to `NULL`.
-#' @param call Passed as the call parameter for [cli::cli_abort] to improve
+#' @param type Name of token; defaults to `NULL`. Optional if token is named.
+#' @param call Passed as the call parameter for [cli::cli_abort()] to improve
 #'   error messages when function is used internally.
 #' @rdname set_access_token
 #' @aliases set_token_type
 #' @export
 #'
+#' @importFrom rlang is_named
 #' @importFrom utils read.table write.table
 set_access_token <- function(token, overwrite = FALSE, install = FALSE, type = NULL, call = caller_env()) {
   check_required(token)
-  check_character(token)
-  check_character(type)
 
-  if (install) {
-    home <- Sys.getenv("HOME")
-    renv <- file.path(home, ".Renviron")
+  if (rlang::is_named(token)) {
+    type <- type %||% names(token)
+    token <- as.character(token)
+  }
 
-    if (!file.exists(renv)) {
-      file.create(renv)
-    } else {
-      # Backup original .Renviron before doing anything else here.
-      file.copy(renv, file.path(home, ".Renviron_backup"))
+  check_character(token, n = 1)
+  check_character(type, n = 1)
 
-      if (any(grepl(type, readLines(renv)))) {
-        if (!overwrite) {
-          cli_abort(
-            c("{.val {type}} already exists in your .Renviron.",
-              "*" = "Set {.arg overwrite = TRUE} to replace this token."
-            ),
-            call = call
-          )
-        } else {
-          oldenv <- read.table(renv, stringsAsFactors = FALSE)
-          newenv <- oldenv[!grepl(type, readLines(renv)), ]
-
-          write.table(
-            newenv, renv,
-            quote = FALSE, sep = "\n",
-            col.names = FALSE, row.names = FALSE
-          )
-
-          cli_inform(
-            c("v" = "Your original .Renviron is backed up to your R HOME directory if needed.")
-          )
-        }
-      }
-    }
-
-    # Append access token to .Renviron file
-    write(glue("{type}=\'{token}\'"), renv, sep = "\n", append = TRUE)
-
+  if (!install) {
     cli_inform(
       c(
-        "i" = "The token {.val {token}} is now stored in your .Renviron.",
-        "*" = 'To use it, restart R or run {.code readRenviron("~/.Renviron")}.
-        You can then access the token using {.code Sys.getenv("{type}")}.'
+        "v" = "Token {.val {type}} set to {.val {token}} with {.fn Sys.setenv}.",
+        "*" = "To use the token in future sessions,
+        run {.fn set_access_token} using {.arg install = TRUE}."
       )
     )
 
-    invisible(return(NULL))
+    Sys.setenv(type = token)
+    return(invisible(NULL))
   }
+
+  home <- Sys.getenv("HOME")
+  renv <- file.path(home, ".Renviron")
+
+  if (file.exists(renv)) {
+    has_type <- any(grepl(type, readLines(renv)))
+
+    if (has_type && !overwrite) {
+      cli_abort(
+        c("{.val {type}} already exists in your {.file .Renviron}.",
+          "*" = "Set {.arg overwrite = TRUE} to replace this token."
+        ),
+        call = call
+      )
+    }
+
+    # Backup original .Renviron before doing anything else here.
+    backup <- file.path(home, ".Renviron_backup")
+    file.copy(renv, backup)
+
+    cli_inform(
+      c("v" = "{.file .Renviron} backed up to {.path {backup}}.")
+    )
+
+    if (has_type) {
+      oldenv <- utils::read.table(renv, stringsAsFactors = FALSE)
+      newenv <- oldenv[!grepl(type, readLines(renv)), ]
+
+      utils::write.table(
+        newenv, renv,
+        quote = FALSE, sep = "\n",
+        col.names = FALSE, row.names = FALSE
+      )
+    }
+  } else {
+    file.create(renv)
+  }
+
+  # Append access token to .Renviron file
+  write(glue("{type}=\'{token}\'"), renv, sep = "\n", append = TRUE)
 
   cli_inform(
     c(
-      "v" = "Token set with {.fun Sys.setenv}.",
-      "*" = "To use the token {.val {token}} in future sessions, run this function with {.arg install = TRUE}."
+      "v" = "Token {.val {token}} saved to .Renviron variable {.val {type}}.",
+      "*" = 'Restart R or run {.code readRenviron("~/.Renviron")} then use
+        {.code Sys.getenv("{type}")} to access the token.'
     )
   )
-
-  Sys.setenv(type = token)
 }
 
 #' @name get_access_token
