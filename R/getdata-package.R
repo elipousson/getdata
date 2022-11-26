d1f24fdc2cc619851243c22f9ac002e5630066fa
Eli Pousson <eli.pousson@gmail.com>
2022-11-25 23:49:56 -0500

    refactor: update NAMESPACE

--- R/getdata-package.R
+++ R/getdata-package.R
@@ -1,11 +1,12 @@
-#' @import rlang
-#' @importFrom glue glue glue_collapse
-#' @importFrom cli cli_abort cli_inform cli_warn
-#' @importFrom sfext st_bbox_ext st_filter_ext df_to_sf is_sf as_sf
-#'   relocate_sf_col rename_sf_col
 #' @keywords internal
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @import rlang
+#' @importFrom rlang has_length has_name
+#' @importFrom glue glue glue_collapse
+#' @importFrom cli cli_abort cli_inform cli_warn
+#' @importFrom sfext st_bbox_ext st_filter_ext df_to_sf is_sf as_sf
+#'   relocate_sf_col rename_sf_col
 ## usethis namespace: end
 rlang::on_load(rlang::local_use_cli())
