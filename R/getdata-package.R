9f27298b29f7409fcc73a7ef49e62fe6544f121a
Eli Pousson <eli.pousson@gmail.com>
2023-01-23 10:12:54 -0500

    refactor: add tidy dependencies + tidy_eval utils

--- R/getdata-package.R
+++ R/getdata-package.R
@@ -3,10 +3,11 @@
 
 ## usethis namespace: start
 #' @import rlang
-#' @importFrom rlang has_length has_name
-#' @importFrom glue glue glue_collapse
 #' @importFrom cli cli_abort cli_inform cli_warn
+#' @importFrom glue glue
+#' @importFrom glue glue glue_collapse
+#' @importFrom lifecycle deprecated
+#' @importFrom rlang has_length has_name
 #' @importFrom sfext st_bbox_ext st_filter_ext df_to_sf is_sf as_sf
 #'   relocate_sf_col rename_sf_col
 ## usethis namespace: end
-rlang::on_load(rlang::local_use_cli())
