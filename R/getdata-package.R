d82155309ee3363ead4964de5fd0d72ff157a854
Eli Pousson <eli.pousson@gmail.com>
2023-03-30 00:07:16 -0400

    refactor: update imports

--- R/getdata-package.R
+++ R/getdata-package.R
@@ -2,16 +2,16 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @import rlang
-#' @importFrom rlang .data `:=` `%||%` is_logical is_null has_name is_string
 #' @importFrom cli cli_abort cli_inform cli_warn
 #' @importFrom glue glue glue_collapse
 #' @importFrom lifecycle deprecated
-#' @importFrom sfext st_bbox_ext
-#' @importFrom sfext st_filter_ext
+#' @importFrom rlang :=
+#' @importFrom rlang .data `%||%` is_logical is_null has_name is_string
+#' @importFrom sfext as_sf
 #' @importFrom sfext df_to_sf
 #' @importFrom sfext is_sf
-#' @importFrom sfext as_sf
 #' @importFrom sfext relocate_sf_col
 #' @importFrom sfext rename_sf_col
+#' @importFrom sfext st_bbox_ext
+#' @importFrom sfext st_filter_ext
 ## usethis namespace: end
